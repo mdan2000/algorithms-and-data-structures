9742732e3d04ae40dd71bd2906b1ad10abdcf606
Danil Miachin <22774695+mdan2000@users.noreply.github.com>
2021-09-04 00:37:08 +0300

    Update BinHeap.cpp

--- data-structures/full/BinHeap.cpp
+++ data-structures/full/BinHeap.cpp
@@ -7,10 +7,24 @@
 #include <list>
 #include <stdexcept>
 
-struct BinHeapNode;
-
 template<typename T>
 class BinHeap {
+private:
+	struct BinHeapNode {
+		T key;
+		std::list<std::shared_ptr<BinHeapNode>> childs;
+		size_t degree = 0;
+	};
+
+	std::shared_ptr<BinHeapNode> MergeEqualHeap(std::shared_ptr<BinHeapNode> l, std::shared_ptr<BinHeapNode> r) const {
+		if (!cmp(l->key, r->key)) {
+			swap(l, r);
+		}
+		l->childs.insert(l->childs.end(), r);
+		l->degree++;
+		return l;
+	}
+
 public:
 	BinHeap() : size_(0) {};
 
@@ -18,11 +32,11 @@ public:
 		size_(0),
 		cmp(cmpFunc) {};
 
-	BinHeap(const std::list<std::shared_ptr<BinHeapNode>>& newChilds,
+	BinHeap(std::list<std::shared_ptr<BinHeapNode>>& newChilds,
 		const std::function<bool(T, T)>& cmpFunc) :
-		Heap_(newChilds),
 		cmp(cmpFunc)
 	{
+		Heap_ = newChilds;
 		size_t newSize_ = 0;
 		for (const auto& c : Heap_) {
 			newSize_ += (1u << c->degree);
@@ -34,69 +48,65 @@ public:
 		std::shared_ptr<BinHeapNode> node = std::make_shared<BinHeapNode>();
 		node->key = newKey;
 		auto it = Heap_.begin();
-		while (it != Heap_.end()) {
-			if ((*it)->degree == node->degree) {
-				if (cmp((*it)->key, node->key)) {
-					std::shared_ptr<BinHeapNode> newNode = *it;
-					(*it)->childs.insert((*it)->childs.end(), node);
-					(*it)->degree++;
-					node = newNode;
-					
-				}
-				else {
-					node->childs.insert(node->childs.end(), *it);
-					node->degree++;
-				}
+		while (it != Heap_.end() && (*it)->degree == node->degree) {
+			if (cmp((*it)->key, node->key)) {
+				(*it)->childs.insert((*it)->childs.end(), node);
+				(*it)->degree++;
+				node = *it;
+				it = Heap_.erase(it);
 			}
-			++it;
+			else {
+				node->childs.insert(node->childs.end(), *it);
+				node->degree++;
+			}
 		}
 		Heap_.insert(it, node);
 		++size_;
 	}
 
-	void Merge(const BinHeap& r) {
+	void Merge(BinHeap& r) {
 		auto l = *this;
 		auto itl = l.Heap_.begin();
 		auto itr = r.Heap_.begin();
 		std::list<std::shared_ptr<BinHeapNode>> newHeap;
 		while (itl != l.Heap_.end() && itr != r.Heap_.end()) {
-			if (itl->degree == itr->degree) {
+			if ((*itl)->degree == (*itr)->degree) {
 				newHeap.insert(newHeap.end(), MergeEqualHeap(*itl, *itr));
-				itl = l->Heap_.erase(itl);
-				itr = r->Heap_.erase(itr);
+				itl = l.Heap_.erase(itl);
+				itr = r.Heap_.erase(itr);
 			}
-			else if (itl->degree == newHeap.back().degree) {
+			else if (!newHeap.empty() && (*itl)->degree == newHeap.back()->degree) {
 				auto node = newHeap.back();
 				newHeap.pop_back();
 				newHeap.insert(newHeap.end(), MergeEqualHeap(node, *itl));
-				itl = l->Heap_.erase(itl);
+				itl = l.Heap_.erase(itl);
 			}
-			else if (itl->degree == newHeap.back().degree) {
+			else if (!newHeap.empty() && (*itr)->degree == newHeap.back()->degree) {
 				auto node = newHeap.back();
 				newHeap.pop_back();
 				newHeap.insert(newHeap.end(), MergeEqualHeap(node, *itr));
-				itr = r->Heap_.erase(itr);
+				itr = r.Heap_.erase(itr);
 			}
 			else {
-				if (cmp(itl->key, itr->key)) {
+				if (cmp((*itl)->key, (*itr)->key)) {
 					newHeap.insert(newHeap.end(), *itl);
-					itl = l->Heap_.erase(itl);
+					itl = l.Heap_.erase(itl);
 				}
 				else {
 					newHeap.insert(newHeap.end(), *itr);
-					itr = r->Heap_.erase(itr);
+					itr = r.Heap_.erase(itr);
 				}
 			}
 		}
 
-		while (itl != l->Heal_.end()) {
+		while (itl != l.Heap_.end()) {
 			newHeap.insert(newHeap.end(), *itl);
-			itl = l->Heap_.erase(itl);
+			itl = l.Heap_.erase(itl);
 		}
 
-		while (itr != r->Heal_.end()) {
+		while (itr != r.Heap_.end()) {
 			newHeap.insert(newHeap.end(), *itr);
-			itr = r->Heap_.erase(itr);
+			itr = r.Heap_.erase(itr);
 		}
 		Heap_ = newHeap;
 	}
@@ -116,7 +126,7 @@ public:
 		return res;
 	}
 
-	void EraseElement() {
+	void Erase() {
 		if (Heap_.empty()) {
 			throw std::runtime_error("Erase from empty BinHeap");
 		}
@@ -132,26 +142,11 @@ public:
 	}
 
 private:
-	struct BinHeapNode {
-		T key;
-		std::list<std::shared_ptr<BinHeapNode>> childs;
-		size_t degree = 0;
-	};
 	size_t size_;
 	std::list<std::shared_ptr<BinHeapNode>> Heap_;
 	std::function<bool(T, T)> cmp = [](const T& l, const T& r) {
 		return l < r;
 	};
-
-	BinHeapNode MergeEqualHeap(const BinHeapNode& l, const BinHeapNode& r) {
-		if (!cmp(l.key, r.key)) {
-			swap(l, r);
-		}
-		l->childs.insert(l->childs.end(), r);
-		l->degree++;
-		return l;
-	}
-	
 };
 
 
@@ -201,9 +196,10 @@ void TestErase() {
 			heap.Add(i);
 		}
 		for (size_t i = 0; i < 9; ++i) {
-			heap.EraseElement();
+			heap.Erase();
 			assert(heap.Get() == i + 1);
 		}
+		std::cout << "Erase OK";
 	}
 }
 
