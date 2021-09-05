a9f0de4dd3ad9814ce1c918fa5a487b88b79f721
Danil Miachin <22774695+mdan2000@users.noreply.github.com>
2021-09-06 02:38:24 +0300

    Update BinHeap.cpp

--- data-structures/full/BinHeap.cpp
+++ data-structures/full/BinHeap.cpp
@@ -35,7 +35,7 @@ public:
         cmp(cmpFunc) {};
 
     void Add(const T& newKey) {
-        
+
         std::shared_ptr<BinHeapNode> node = std::make_shared<BinHeapNode>();
         node->key = newKey;
         auto it = Heap_.begin();
@@ -64,21 +64,20 @@ public:
     }
 
     void Merge(std::list<std::shared_ptr<BinHeapNode>>& r) {
-        auto l = *this;
-        auto itl = l.Heap_.begin();
+        auto itl = Heap_.begin();
         auto itr = r.begin();
         std::list<std::shared_ptr<BinHeapNode>> newHeap;
-        while (itl != l.Heap_.end() && itr != r.end()) {
+        while (itl != Heap_.end() && itr != r.end()) {
             if ((*itl)->degree == (*itr)->degree) {
                 newHeap.insert(newHeap.end(), MergeEqualHeap(*itl, *itr));
-                itl = l.Heap_.erase(itl);
+                itl = Heap_.erase(itl);
                 itr = r.erase(itr);
             }
             else if (!newHeap.empty() && (*itl)->degree == newHeap.back()->degree) {
                 auto node = newHeap.back();
                 newHeap.pop_back();
                 newHeap.insert(newHeap.end(), MergeEqualHeap(node, *itl));
-                itl = l.Heap_.erase(itl);
+                itl = Heap_.erase(itl);
             }
             else if (!newHeap.empty() && (*itr)->degree == newHeap.back()->degree) {
                 auto node = newHeap.back();
@@ -89,7 +88,7 @@ public:
             else {
                 if (cmp((*itl)->key, (*itr)->key)) {
                     newHeap.insert(newHeap.end(), *itl);
-                    itl = l.Heap_.erase(itl);
+                    itl = Heap_.erase(itl);
                 }
                 else {
                     newHeap.insert(newHeap.end(), *itr);
@@ -98,9 +97,9 @@ public:
             }
         }
 
-        while (itl != l.Heap_.end()) {
+        while (itl != Heap_.end()) {
             newHeap.insert(newHeap.end(), *itl);
-            itl = l.Heap_.erase(itl);
+            itl = Heap_.erase(itl);
         }
 
         while (itr != r.end()) {
@@ -150,7 +149,6 @@ public:
                 for (const auto& cnode : q.front()->childs) {
                     q.push(cnode);
                 }
-                std::cout << q.front()->key << ' ';
                 q.front().reset();
                 q.pop();
             }
@@ -168,90 +166,90 @@ private:
 
 
 void TestAdd() {
-	{
-		BinHeap<size_t> heap;
-		assert(heap.size() == 0);
-	}
-	{
-		BinHeap<size_t> heap;
-		for (size_t i = 0u; i < 10u; ++i) {
-			heap.Add(i);
-		}
-		assert(heap.size() == 10);
-	}
-	{
-		BinHeap<size_t> heap;
-		heap.Add(1);
-		heap.Add(2);
-		assert(heap.size() == 2);
-	}
-	std::cout << "Add OK" << std::endl;
+    {
+        BinHeap<size_t> heap;
+        assert(heap.size() == 0);
+    }
+    {
+        BinHeap<size_t> heap;
+        for (size_t i = 0u; i < 10u; ++i) {
+            heap.Add(i);
+        }
+        assert(heap.size() == 10);
+    }
+    {
+        BinHeap<size_t> heap;
+        heap.Add(1);
+        heap.Add(2);
+        assert(heap.size() == 2);
+    }
+    std::cout << "Add OK" << std::endl;
 }
 
 void TestGet() {
-	{
-		BinHeap<size_t> heap;
-		for (size_t i = 0; i < 10; ++i) {
-			heap.Add(i);
-			assert(0 == heap.Get());
-		}
-	}
-	{
-		BinHeap<size_t> heap;
-		for (size_t i = 10; i > 1; --i) {
-			heap.Add(i - 1);
-			assert(i - 1 == heap.Get());
-		}
-	}
-	std::cout << "Get OK" << std::endl;
+    {
+        BinHeap<size_t> heap;
+        for (size_t i = 0; i < 10; ++i) {
+            heap.Add(i);
+            assert(0 == heap.Get());
+        }
+    }
+    {
+        BinHeap<size_t> heap;
+        for (size_t i = 10; i > 1; --i) {
+            heap.Add(i - 1);
+            assert(i - 1 == heap.Get());
+        }
+    }
+    std::cout << "Get OK" << std::endl;
 }
 
 void TestErase() {
-	{
-		BinHeap<size_t> heap;
-		for (size_t i = 0; i < 10; ++i) {
-			heap.Add(i);
-		}
-		for (size_t i = 0; i < 9; ++i) {
-			heap.Erase();
-			assert(heap.Get() == i + 1);
-		}
-		std::cout << "Erase OK" << std::endl;
-	}
+    {
+        BinHeap<size_t> heap;
+        for (size_t i = 0; i < 10; ++i) {
+            heap.Add(i);
+        }
+        for (size_t i = 0; i < 9; ++i) {
+            heap.Erase();
+            assert(heap.Get() == i + 1);
+        }
+        std::cout << "Erase OK" << std::endl;
+    }
 }
 
 void TestElseCMP() {
-	{
-		BinHeap<size_t> heap([](const size_t& l, const size_t& r) { return l > r; });
-		
-		for (size_t i = 0; i < 10; ++i) {
-			heap.Add(i);
-			assert(heap.Get() == i);
-		}
-	}
+    {
+        BinHeap<size_t> heap([](const size_t& l, const size_t& r) { return l > r; });
 
-	std::cout << "TestElseCMP OK" << std::endl;
+        for (size_t i = 0; i < 10; ++i) {
+            heap.Add(i);
+            assert(heap.Get() == i);
+        }
+    }
+
+    std::cout << "TestElseCMP OK" << std::endl;
 }
 
 void TestMem() {
-	{
-		BinHeap<size_t> heap;
-		for (size_t i = 0; i < 10000; ++i) {
-			heap.Add(i);
-		}
-	}
-	{
-		BinHeap<size_t> heap;
-		for (size_t i = 0; i < 100000; ++i) {
-			heap.Add(i);
-		}
-	}
+    {
+        BinHeap<size_t> heap;
+        for (size_t i = 0; i < 10000; ++i) {
+            heap.Add(i);
+        }
+    }
+    {
+        BinHeap<size_t> heap;
+        for (size_t i = 0; i < 100000; ++i) {
+            heap.Add(i);
+        }
+    }
 }
 
 void TestBinHeap() {
-	TestAdd();
-	TestGet();
-	TestErase();
-	TestElseCMP();
-	TestMem();
+    TestAdd();
+    TestGet();
+    TestErase();
+    TestElseCMP();
+    TestMem();
 }
